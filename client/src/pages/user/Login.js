ff0c451cb6c1f0caee96f39158f3f63232012f27
Anurag Bhattacharjee <anuragbhattacharjee612@gmail.com>
2025-06-14 12:54:26 +0530

    updated login and signup pages on landing page added login and signup bg image into assets

--- client/src/pages/user/Login.js
+++ client/src/pages/user/Login.js
@@ -1,32 +1,33 @@
-import logo from '../../assets/logo.png';
-import React, { useState } from 'react';
-import axios from 'axios';
-import './login.css'
-import { useNavigate } from 'react-router-dom';
+import login_bg from "../../assets/login_bg.jpg";
+import React, { useState } from "react";
+import axios from "axios";
+import "./login.css";
+import { useNavigate } from "react-router-dom";
+import { GoogleLogo, AppleLogo, SquaresFour } from "@phosphor-icons/react";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:4000/api/login', {
+      const response = await axios.post("http://localhost:4000/api/login", {
         email: email,
-        password: password
+        password: password,
       });
-      console.log('Signup successful:', response.data);
+      console.log("Signup successful:", response.data);
       // Add your logic after successful signup, such as redirecting to another page
     } catch (error) {
-      console.error('Error signing up:', error);
+      console.error("Error signing up:", error);
       // Handle signup error, such as displaying an error message to the user
     }
   };
 
   return (
     <div
-      className="modal fade"
+      className="modal fade h-[96vh] overflow-hidden"
       id="loginModal"
       tabIndex={-1}
       aria-labelledby="loginModalLabel"
@@ -35,47 +36,83 @@ function Login() {
       <div className="modal-dialog modal-xl modal-dialog-centered">
         <div className="modal-content">
           <div className="d-flex position-relative">
-            <img
-              className="bgimage"
-              src={logo}
-              alt="Background"
-            />
-            <div className="right-column position-absolute w-50 opacity-75 h-100 ms-auto end-0">
-              <div className="form-container">
-                <h2 className="title">Sign in</h2>
+            <img className="bgimage" src={login_bg} alt="Background" />
+            <div className="absolute top-[40%] left-[15%] text-5xl">
+              Printopher
+            </div>
+            <div className="right-column position-absolute w-50 h-100 ms-auto end-0 bg-black/65">
+              <div className="form-container w-[450px]">
+                {/* <h2 className="title">Sign in</h2> */}
                 <form onSubmit={handleSubmit} className="form">
                   <input
                     type="email"
                     placeholder="Email Address"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="input"
+                    className="input h-[6vh] mb-[30px] rounded-[12px] bg-white"
                     required
-                    style={{color: 'black', fontWeight: 'bolder'}}
+                    style={{ color: "black", fontWeight: "bolder" }}
                   />
                   <input
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="input"
+                    className="input h-[6vh] mb-[30px] rounded-[12px] bg-white"
                     required
-                    style={{color: 'black', fontWeight: 'bolder'}}
+                    style={{ color: "black", fontWeight: "bolder" }}
                   />
-                  <div className="checkbox-container">
+                  {/* <div className="checkbox-container">
                     <input type="checkbox" id="remember" className="checkbox" />
                     <label htmlFor="remember" className="label">
                       Remember me
                     </label>
-                  </div>
+                  </div> */}
 
-                  <button type="submit" className="but"  onClick={() => navigate('/dashboard')}>
-                    <div className="button">
-                      Sign In
+                  <button
+                    type="submit"
+                    className="w-[20vw]"
+                    onClick={() => navigate("/dashboard")}
+                  >
+                    <div className="button h-12 w-full text-xl font-extrabold">
+                      Login
                     </div>
                   </button>
 
-                  <div className="link-container">
+                  <p className="text-gray-300 text-sm my-4">or login with</p>
+
+                  <div className="flex justify-center items-center gap-4 mb-4">
+                    <button
+                      type="button"
+                      className="border p-[12px] rounded-[6px] bg-[#faf3dd]"
+                    >
+                      <GoogleLogo className="bg-black rounded-full" size={26} />
+                    </button>
+                    <button
+                      type="button"
+                      className="border p-[12px] rounded-[6px] bg-[#faf3dd]"
+                    >
+                      <AppleLogo
+                        className="rounded-full"
+                        size={26}
+                        color="black"
+                        weight="fill"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      className="border p-[12px] rounded-[6px] bg-[#faf3dd]"
+                    >
+                      <SquaresFour
+                        className="rounded-full"
+                        size={26}
+                        color="black"
+                        weight="fill"
+                      />
+                    </button>
+                  </div>
+
+                  <div className="link-container flex flex-col gap-1">
                     <a href="#" className="link">
                       Forgot password?
                     </a>
--- client/src/pages/user/Signup.js
+++ client/src/pages/user/Signup.js
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import logo from '../../assets/logo.png';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import signup_bg from "../../assets/signup_bg.jpg";
+import { GoogleLogo, AppleLogo, SquaresFour } from "@phosphor-icons/react";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
-  const [name_, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name_, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const navigate = useNavigate(); // Changed variable name
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:4000/api/signup', {
+      const response = await axios.post("http://localhost:4000/api/signup", {
         name: name_,
         email: email,
-        password: password
+        password: password,
+        confirmPassword: confirmPassword,
       });
-      
-      console.log('Signup successful:', response.data);
+
+      console.log("Signup successful:", response.data);
       // Redirect to dashboard after successful signup
-      navigate('/dashboard');
+      navigate("/dashboard");
     } catch (error) {
-      console.error('Error signing up:', error);
+      console.error("Error signing up:", error);
       // Handle signup error, such as displaying an error message to the user
     }
   };
@@ -38,58 +41,104 @@ function Signup() {
       <div className="modal-dialog modal-xl modal-dialog-centered">
         <div className="modal-content">
           <div className="d-flex position-relative">
-            <img
-              className="bgimage"
-              src={logo}
-              alt="Background"
-            />
-            <div className="right-column position-absolute w-50 opacity-75 h-100 ms-auto end-0">
-              <div className="form-container">
-                <h2 className="title">Sign in</h2>
+            <img className="bgimage" src={signup_bg} alt="Background" />
+            <div className="right-column position-absolute w-50 h-100 bg-black/65 ms-auto end-0">
+              <div className="form-container w-[450px]">
+                {/* <h2 className="title">Sign in</h2> */}
                 <form onSubmit={handleSubmit} className="form">
                   <input
                     type="text"
                     placeholder="Full name"
                     value={name_}
                     onChange={(e) => setName(e.target.value)}
-                    className="input"
+                    className="input h-[6vh] mb-[30px] rounded-[12px] bg-white"
                     required
-                    style={{color: 'black', fontWeight: 'bolder'}}
+                    style={{ color: "black", fontWeight: "bolder" }}
                   />
                   <input
                     type="email"
                     placeholder="Email Address"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="input"
+                    className="input h-[6vh] mb-[30px] rounded-[12px] bg-white"
                     required
-                    style={{color: 'black', fontWeight: 'bolder'}}
+                    style={{ color: "black", fontWeight: "bolder" }}
                   />
                   <input
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="input"
+                    className="input h-[6vh] mb-[30px] rounded-[12px] bg-white"
                     required
-                    style={{color: 'black', fontWeight: 'bolder'}}
+                    style={{ color: "black", fontWeight: "bolder" }}
                   />
-                  <div className="checkbox-container">
+                  <input
+                    type="password"
+                    placeholder="Rewrite Password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    className="input h-[6vh] mb-[30px] rounded-[12px] bg-white"
+                    required
+                    style={{ color: "black", fontWeight: "bolder" }}
+                  />
+                  {/* <div className="checkbox-container">
                     <input type="checkbox" id="remember" className="checkbox" />
                     <label htmlFor="remember" className="label">
                       Remember me
                     </label>
-                  </div>
-                  <button type="submit" className="button" style={{ background: '#fff', color: 'black' }} onClick={() => navigate('/dashboard')}>Sign up</button>
+                  </div> */}
+                  <button
+                    type="submit"
+                    className="button h-12 w-[20vw] bg-[#faf3dd] text-xl font-extrabold text-black"
+                    onClick={() => navigate("/dashboard")}
+                  >
+                    Sign up
+                  </button>
 
-                  <div className="link-container">
-                    <a href="#" className="link">
-                      Forgot password?
-                    </a>
-                    <a href="#" className="link">
-                      Don't have an account? Sign Up
-                    </a>
+                  <p className="text-gray-300 text-sm my-4">or sign up with</p>
+
+                  <div className="flex justify-center items-center gap-4 mb-4">
+                    <button
+                      type="button"
+                      className="border p-[12px] rounded-[6px] bg-[#faf3dd]"
+                    >
+                      <GoogleLogo className="bg-black rounded-full" size={26} />
+                    </button>
+                    <button
+                      type="button"
+                      className="border p-[12px] rounded-[6px] bg-[#faf3dd]"
+                    >
+                      <AppleLogo
+                        className="rounded-full"
+                        size={26}
+                        color="black"
+                        weight="fill"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      className="border p-[12px] rounded-[6px] bg-[#faf3dd]"
+                    >
+                      <SquaresFour
+                        className="rounded-full"
+                        size={26}
+                        color="black"
+                        weight="fill"
+                      />
+                    </button>
                   </div>
+
+                  <p className="text-center text-gray-300 text-sm">
+                    By creating an account you agree to Printopher's{" "}
+                    <a href="#" className="text-[#faf3dd] font-extrabold">
+                      Terms of Services
+                    </a>{" "}
+                    and{" "}
+                    <a href="#" className="text-[#faf3dd] font-extrabold">
+                      Privacy Policy
+                    </a>
+                  </p>
                 </form>
               </div>
             </div>
